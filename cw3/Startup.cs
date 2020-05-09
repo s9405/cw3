793fe7da59ded3efb0f4bd8d2d3dd69efbfc1edf
s9405 <s9405@pjwstk.edu.pl>
2020-05-09 19:48:57 +0200

    cwiczenia 6 zad 1

--- cw3/Startup.cs
+++ cw3/Startup.cs
@@ -1,9 +1,11 @@
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using cw3.DAL;
 using cw3.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -35,6 +37,39 @@ namespace cw3
                 app.UseDeveloperExceptionPage();
             }
             app.UseHttpsRedirection();
+
+
+            app.Use(async (context, next) =>
+            {
+                if (!context.Request.Headers.ContainsKey("Index") && context.Request.Headers["Index"].ToString().Equals("") 
+                && context.Request.Headers["Index"].ToString()==null)
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    return;
+                }
+                string index = context.Request.Headers["Index"].ToString();
+                using ( var connection = new SqlConnection("Data Source=db-mssql;Initial Catalog=s9405;Integrated Security=true"))
+                using (var command = new SqlCommand())
+                {
+                    connection.Open();
+                    command.Connection = connection;
+                    var transaction = connection.BeginTransaction();
+                    command.Transaction = transaction;
+                    command.Parameters.AddWithValue("index", index);
+                    command.CommandText = "Select * FROM student where indexnumber = @index";
+                    var dr = command.ExecuteReader();
+
+                    if (!dr.HasRows)
+                    {
+                        dr.Close();
+                        transaction.Rollback();
+                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                        await context.Response.WriteAsync("Nie ma takiego indexu w bazie");
+                        return;
+                    }
+                }
+                await next();
+            });
             app.UseRouting();
             app.UseAuthorization();
             app.UseEndpoints(endpoints =>
